c9844a039d523f76dccdc44f825508f97701c456
gatekeeper.tvsw <gatekeeper.tvsw@lge.com>
2017-01-06 14:30:47 +0900

    Change default repo_url

--- bin/run.sh
+++ bin/run.sh
@@ -3,13 +3,21 @@ function realpath() {
 	python -c "import os; r = os.path.abspath('"$1"');print(r)"
 }
 
-if [ -z "${BUILD_LAYER}" ]; then
-	echo "ERROR: define BUILD_LAYER (git url )"
-	exit 1
+if [ -z "${REPO_URL}" ]; then
+    REPO_URL="https://github.com/hishamhm/htop"
+	echo "ERROR: You don't define REPO_URL (git url )"
+	echo "ERROR: Default = ${REPO_URL}"
 fi
-set -x
+
+if [ -z "${GIT_BRANCH}" ]; then
+    GIT_BRANCH="master"
+fi
+
+# Find the script's location
 BASEDIR=$(dirname "$0")
 BASEDIR=`realpath ${BASEDIR}`
+
+# Find Tomcat's location
 TOMCAT_HOME=`dirname $(dirname $(which catalina.sh))`
 if [ "${TOMCAT_HOME}" = "" ]; then
 	echo "Cant' find 'catalina.sh'"
@@ -20,6 +28,8 @@ else
 fi
 echo "Tomcat home: ${TOMCAT_HOME}"
 echo "Base dir: ${BASEDIR}"
+
+#
 echo "Web context: "
 read  WEB_CONTEXT
 echo "OpenGrok port: "
@@ -47,10 +57,9 @@ echo "Wait for 3 seconds"
 sleep 3
 pushd ${OPENGROK_INSTANCE_BASE}/src
 if [ -z "${GIT_BRANCH}" ]; then
- echo "INFO: No sources"
+    echo "INFO: No sources"
 else
- git clone -b ${GIT_BRANCH} ${BUILD_LAYER} "build-starfish-${GIT_BRANCH//@}"
-
+    git clone -b ${GIT_BRANCH} ${REPO_URL} "$(basename ${REPO_URL})-${GIT_BRANCH//@}"
 fi
 popd
 echo "WAIT..."
@@ -61,6 +70,20 @@ echo "export OPENGROK_DISTRIBUTIOIN_BASE=\"${OPENGROK_DISTRIBUTIOIN_BASE}\"" >> 
 echo "export OPENGROK_INSTANCE_BASE=\"${OPENGROK_INSTANCE_BASE}\"" >> set_${WEB_CONTEXT}.sh
 echo "export OPENGROK_PORT=\"${OPENGROK_PORT}\"" >> set_${WEB_CONTEXT}.sh
 echo "export SCRIPT_DIRECTORY=\"${SCRIPT_DIRECTORY}\"" >> set_${WEB_CONTEXT}.sh
-echo "export PATH=\"${SCRIPT_DIRECTORY}\"/bin:${PATH}\" ">> set_${WEB_CONTEXT}.sh
-echo "Done"
+echo "export OPENGROK_WEBAPP_CONTEXT=\"${WEB_CONTEXT}\"" >> set_${WEB_CONTEXT}.sh
+echo "export PATH=\"${SCRIPT_DIRECTORY}/bin:${PATH}\" ">> set_${WEB_CONTEXT}.sh
+cp set_${WEB_CONTEXT}.sh ${OPENGROK_INSTANCE_BASE}/
+
+# Print information
+echo ""
+echo "# Information "
+echo "OPENGROK_DISTRIBUTIOIN_BASE   = ${OPENGROK_DISTRIBUTIOIN_BASE}"
+echo "OPENGROK_INSTANCE_BASE        = $(realpath ${OPENGROK_INSTANCE_BASE})"
+echo "OPENGROK_PORT                 = ${OPENGROK_PORT}"
+echo "SCRIPT_DIRECTORY              = ${SCRIPT_DIRECTORY}"
+echo "OPENGROK_WEBAPP_CONTEXT       = /${WEB_CONTEXT}"
+echo "TOMCAT_HOME                   = ${TOMCAT_HOME}"
+
+echo ""
+echo "# Done"
 set +x
