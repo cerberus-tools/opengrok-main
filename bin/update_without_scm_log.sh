5413b37a18dcc3c64c07c0ef201b64cce7aa7991
Sunjoo Park <allessunjoo.park@lge.com>
2017-01-10 11:41:54 +0900

    Update ignore patterns.

--- bin/update_without_scm_log.sh
+++ bin/update_without_scm_log.sh
@@ -5,7 +5,7 @@ java \
 -Dorg.opensolaris.opengrok.history.git="$(which git)" \
 -Djava.util.logging.config.file=${OPENGROK_INSTANCE_BASE}/logging.properties \
 -jar ${OPENGROK_DISTRIBUTION_BASE}/opengrok.jar \
--i */chromium*/src/chrome/test -i */git.indirectionsymlink -i */git.indirectionsymlink/* -i */buildhistory -i */BUILD/buildstats/* -i */BUILD/cache/* -i */BUILD/log/* -i */BUILD/stamps/* -i */BUILD/sstate-control/* -i */BUILD/sysroots/* -i */python2.7/* -i *.done -i *.tar.* -i *.zip -i */git2* -i */git2/* -i */downloads/* -i */sstate-cache/* -i *.a -i *.so -i *.o -i *bus\/devices* -i *.ipk -i *.jar -i 'devices/*.0' -i '/temp/*' -i *.pyc -i *.pyo -i *.pyd -i .git -i */.git -i */temp -i __pycache__ -i */__pycache__  -i *toolchain* \
+-i */glge-*js -i */chromium*/src/chrome/test -i */git.indirectionsymlink -i */git.indirectionsymlink/* -i */buildhistory -i */BUILD/buildstats/* -i */BUILD/cache/* -i */BUILD/log/* -i */BUILD/stamps/* -i */BUILD/sstate-control/* -i */BUILD/sysroots/* -i */python2.7/* -i *.done -i *.tar.* -i *.zip -i */git2* -i */git2/* -i */downloads/* -i */sstate-cache/* -i *.a -i *.so -i *.o -i *bus\/devices* -i *.ipk -i *.jar -i 'devices/*.0' -i '/temp/*' -i *.pyc -i *.pyo -i *.pyd -i .git -i */.git -i */temp -i __pycache__ -i */__pycache__  -i *toolchain* \
 -m 256 \
 -r on \
 -a on \
