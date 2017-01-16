511db1a0a3a110d0eb13998e8af08f673b2197cb
gatekeeper.tvsw <gatekeeper.tvsw@lge.com>
2017-01-16 15:40:09 +0900

    Add '-i */second_batch_js_heavy.*' option
    Add '-i */second_batch_js_heavy.*' option not to add a file
    second_batch_js_heavy.min.js to an index database. This file has been
    stored as minified format. OpenGrok 0.12.1.6 seems not to parse this
    file correctly. When OpenGrok tries to parse this file, OOM in heap area
    is always occured
    
    :Issues:
    [WEBOSDWBS-9292] OOM is occured when indexing chromium44

--- bin/update_without_scm_log.sh
+++ bin/update_without_scm_log.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
+set -x
 java \
--Xmx10g \
+-Xmx30g \
 -Dcom.sun.management.jmxremote.port=1997 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false \
 -Dorg.opensolaris.opengrok.history.Subversion="$(which svn)" \
 -Dorg.opensolaris.opengrok.history.git="$(which git)" \
 -Djava.util.logging.config.file=${OPENGROK_INSTANCE_BASE}/logging.properties \
 -jar ${OPENGROK_DISTRIBUTION_BASE}/opengrok.jar \
--i */glge-*js -i */chromium*/src/chrome/test -i */git.indirectionsymlink -i */git.indirectionsymlink/* -i */buildhistory -i */BUILD/buildstats/* -i */BUILD/cache/* -i */BUILD/log/* -i */BUILD/stamps/* -i */BUILD/sstate-control/* -i */BUILD/sysroots/* -i */python2.7/* -i *.done -i *.tar.* -i *.zip -i */git2* -i */git2/* -i */downloads/* -i */sstate-cache/* -i *.a -i *.so -i *.o -i *bus\/devices* -i *.ipk -i *.jar -i 'devices/*.0' -i '/temp/*' -i *.pyc -i *.pyo -i *.pyd -i .git -i */.git -i */temp -i __pycache__ -i */__pycache__  -i *toolchain* \
+-i */second_batch_js_heavy.* \
+-i *.exe -i *.msi -i *.utf8 -i */glge-*js -i */chromium*/src/chrome/test -i */git.indirectionsymlink -i */git.indirectionsymlink/* -i */buildhistory -i */BUILD/buildstats/* -i */BUILD/cache/* -i */BUILD/log/* -i */BUILD/stamps/* -i */BUILD/sstate-control/* -i */BUILD/sysroots/* -i */python2.7/* -i *.done -i *.tar.* -i *.zip -i */git2* -i */git2/* -i */downloads/* -i */sstate-cache/* -i *.a -i *.so -i *.o -i *bus\/devices* -i *.ipk -i *.jar -i 'devices/*.0' -i '/temp/*' -i *.pyc -i *.pyo -i *.pyd -i .git -i */.git -i */temp -i __pycache__ -i */__pycache__  -i *toolchain* \
 -m 256 \
 -r on \
 -a on \
@@ -16,3 +19,4 @@ java \
 -s ${OPENGROK_INSTANCE_BASE}/src \
 -d ${OPENGROK_INSTANCE_BASE}/data \
 -C
+set +x
