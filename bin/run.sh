dc83f20cd882b1063992f719317eab10e5770d1b
Sunjoo Park <allessunjoo.park@lge.com>
2019-04-09 11:16:07 +0900

    Upgrade opegrok to 1.2.6

--- bin/run.sh
+++ bin/run.sh
@@ -53,6 +53,7 @@ fi
 
 echo "INFO: Make SRC_ROOT and DATA_ROOT directories"
 mkdir -p ${OPENGROK_INSTANCE_BASE}/src
+mkdir -p ${OPENGROK_INSTANCE_BASE}/etc
 mkdir -p ${OPENGROK_INSTANCE_BASE}/data
 
 TARGET_WAR="${BASEDIR}/../lib/${WEB_CONTEXT}.war"
@@ -63,30 +64,24 @@ fi
 
 echo "INFO: Change WEB-INF/web.xml in ${TARGET_WAR}"
 cp $BASEDIR/../lib/source.war ${TARGET_WAR}
+rm -rf work
 mkdir work
-cp -r web/* work
+cp -rv web/* work
 pushd work
 #jar xf ${TARGET_WAR} WEB-INF/web.xml
 sed -i -e  "s|    <param-value>/var/opengrok/etc/configuration.xml</param-value>|    <param-value>${OPENGROK_INSTANCE_BASE}/etc/configuration.xml</param-value>|g" WEB-INF/web.xml
 sed -i -e "s|2424|${OPENGROK_PORT}|g" WEB-INF/web.xml
 sed -i -e "s|@CONNECTIONPASSWORD@|${CONNECTIONPASSWORD}|g" WEB-INF/web.xml
 sed -i -e "s|@CONNECTIONPASSWORD@|${CONNECTIONPASSWORD}|g" META-INF/context.xml
-#jar uf ${TARGET_WAR} WEB-INF/web.xml
+jar uf ${TARGET_WAR} WEB-INF/web.xml
 jar uf ${TARGET_WAR} *
 popd
-echo "INFO: Change menu.jspf"
-jar xf ${TARGET_WAR}  menu.jspf
-sed -i -e "s|Math.min(6, |Math.min(20, |g" menu.jspf
-jar uf ${TARGET_WAR}  menu.jspf
-rm -rf menu.jspf*
-rm -rf WEB-INF*
-
-echo "INFO: Deploy a war file to Tomcat: ${TOMCAT_HOME}"
-cp ${TARGET_WAR} ${TOMCAT_HOME}/webapps/
-#WEB_CONTEXT=${WEB_CONTEXT} OPENGROK_PORT=${OPENGROK_PORT} $BASEDIR/OpenGrok deploy
-
-echo "INFO: Wait for 1 seconds"
-sleep 1
+#echo "INFO: Change menu.jspf"
+#jar xf ${TARGET_WAR}  menu.jspf
+#sed -i -e "s|Math.min(6, |Math.min(20, |g" menu.jspf
+#jar uf ${TARGET_WAR}  menu.jspf
+#rm -rf menu.jspf*
+#rm -rf WEB-INF*
 echo "INFO: Get a sample project codes to a first indexing job"
 pushd ${OPENGROK_INSTANCE_BASE}/src
 if [ -z "${GIT_BRANCH}" ]; then
@@ -97,8 +92,22 @@ fi
 popd
 echo "INFO: WAIT..."
 sleep 1
-WEB_CONTEXT=${WEB_CONTEXT} OPENGROK_PORT=${OPENGROK_PORT} $BASEDIR/OpenGrok index
 
+WEB_CONTEXT=${WEB_CONTEXT} OPENGROK_PORT=${OPENGROK_PORT} java \
+-Djava.util.logging.config.file=${OPENGROK_INSTANCE_BASE}/logging.properties \
+-jar $OPENGROK_DISTRIBUTION_BASE/opengrok.jar \
+-W ${OPENGROK_INSTANCE_BASE}/etc/configuration.xml \
+-s ${OPENGROK_INSTANCE_BASE}/src \
+-d ${OPENGROK_INSTANCE_BASE}/data \
+-P -H -S -G -c /usr/local/bin/ctags
+
+echo "INFO: Deploy a war file to Tomcat: ${TOMCAT_HOME}"
+cp ${TARGET_WAR} ${TOMCAT_HOME}/webapps/
+#WEB_CONTEXT=${WEB_CONTEXT} OPENGROK_PORT=${OPENGROK_PORT} $BASEDIR/OpenGrok deploy
+
+echo "INFO: Wait for 1 seconds"
+sleep 1
+#WEB_CONTEXT=${WEB_CONTEXT} OPENGROK_PORT=${OPENGROK_PORT} $BASEDIR/OpenGrok index
 echo "export OPENGROK_DISTRIBUTION_BASE=\"${OPENGROK_DISTRIBUTION_BASE}\"" > set_${WEB_CONTEXT}.sh
 echo "export OPENGROK_INSTANCE_BASE=\"${OPENGROK_INSTANCE_BASE}\"" >> set_${WEB_CONTEXT}.sh
 echo "export OPENGROK_PORT=\"${OPENGROK_PORT}\"" >> set_${WEB_CONTEXT}.sh
